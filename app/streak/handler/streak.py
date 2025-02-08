75744c1eda0535fa7e77fd73d1a8fa9d477a0d63
Lucas-Vini <lvinicius31@gmail.com>
2025-02-08 00:12:21 -0300

    creating handler class

---
+++ app/streak/handler/streak.py
@@ -0,0 +1,3 @@
+class StreakHandler():
+    def __init__(username: str) -> dict: 
+        pass
--- app/streak/views.py
+++ app/streak/views.py
@@ -1,8 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from app.streak.handler.streak import StreakHandler
 
 streak = Blueprint("streak", __name__)
 
 @streak.route("/streak")
 def main():
 	'''Streak endpoint, used to know how many day streaks user has'''
-	return "No data about day streaks yet"
+	username = request.args.get("username")
+	data = StreakHandler(username)
+	return data
