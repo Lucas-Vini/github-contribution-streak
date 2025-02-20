3232200ec45d6aed43c0b4216bd6cc5c105f7297
Lucas-Vini <lvinicius31@gmail.com>
2025-02-20 14:56:06 -0300

    checking if username is string

--- app/streak/handler/streak.py
+++ app/streak/handler/streak.py
@@ -6,3 +6,8 @@ class StreakHandler():
     def get_streaks(self):
         if not self._is_valid_username():
             return {"error": self.message}, 422
+        
+    def _is_valid_username(self):
+        if not isinstance(self.username, str):
+            self.message = "Username must be string"
+            return False
